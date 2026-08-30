b937890fbfa672664043da05a1086d706ddd566c
donthub <dontsmail@gmail.com>
2026-08-30 23:57:38 +0200

    Add fade animation

--- web/src/components/PlayerActionPanel.jsx
+++ web/src/components/PlayerActionPanel.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 export default function PlayerActionPanel({ player, character, action }) {
+    const [previousAction, setPreviousAction] = React.useState(null);
+    const [renderedAction, setRenderedAction] = React.useState(action);
+
+    if (renderedAction !== action) {
+        setPreviousAction(renderedAction);
+        setRenderedAction(action);
+    }
+
     return (
         <div className={`player-action-container ${player}-action`}>
-            {action === null ? null : <img alt={`${character.name} move`} src={action}/>}
+            {previousAction === null ? null :
+                <img key={`previous-${previousAction}`}
+                     className="player-action-image player-action-previous"
+                     alt=""
+                     src={previousAction}
+                     onAnimationEnd={() => setPreviousAction(null)}/>}
+            {action === null ? null :
+                <img key={`current-${action}`}
+                     className="player-action-image player-action-current"
+                     alt={`${character.name} move`}
+                     src={action}/>}
         </div>
     );
 }
